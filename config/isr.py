fb6f08a9c798e4c471559d28433f13b719202033
Jim Chiang <jchiang@slac.stanford.edu>
2018-10-08 23:10:53 -0700

    fix config entries

--- config/isr.py
+++ config/isr.py
@@ -10,5 +10,5 @@ config.doCrosstalk = True
 config.doAddDistortionModel = False
 
 # Work-around for median-bug in overscan correction (DM-15203).
-config.isr.overscanFitType = 'POLY'
-config.isr.overscanOrder = 0
+config.overscanFitType = 'POLY'
+config.overscanOrder = 0
